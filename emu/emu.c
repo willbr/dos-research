347bd5392a0ad0837b9bc1bf787c8ee159c9af76
William Bettridge-Radford <william.bettridge.radford@gail.com>
2024-05-04 10:27:43 +0100

    sketch 8088 emulator

---
+++ emu/emu.c
@@ -0,0 +1,471 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#define false 0
+#define true (!false)
+
+#define mask_flag_z 0x80
+#define mask_flag_h 0x40
+#define mask_flag_n 0x20
+#define mask_flag_c 0x10
+
+#define computer_set_flag(gb, flag) \
+		(gb->af.part.f |= flag)
+
+#define computer_clear_flag(gb, flag) \
+		(gb->af.part.f &= ~(flag))
+
+typedef unsigned char byte;
+
+typedef unsigned char  u8;
+typedef   signed char  s8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef unsigned long long u64;
+
+
+#if defined(LITTLE_ENDIAN)
+struct RegisterAX { u8 ah; u8 al; };
+struct RegisterBX { u8 bh; u8 bl; };
+struct RegisterCX { u8 ch; u8 cl; };
+struct RegisterDX { u8 dh; u8 dl; };
+#elif defined(BIG_ENDIAN)
+struct RegisterAX { u8 ah; u8 al; };
+struct RegisterBX { u8 bh; u8 bl; };
+struct RegisterCX { u8 ch; u8 cl; };
+struct RegisterDX { u8 dh; u8 dl; };
+#else
+#error "Please define LITTLE_ENDIAN or BIG_ENDIAN"
+#endif
+
+struct Computer;
+typedef int (*interupt_callback)(struct Computer*);
+
+typedef struct Computer {
+	union { u16 u16; struct RegisterAX part; } ax; /* accumulator */
+	union { u16 u16; struct RegisterBX part; } bx; /* address     */
+	union { u16 u16; struct RegisterCX part; } cx; /* coutner     */
+	union { u16 u16; struct RegisterDX part; } dx; /* 32bit extention of ax */
+	u16 si; /* source address */
+	u16 di; /* target address */
+	u16 bp; /* base pointer   */
+	u16 sp; /* stack pointer  */
+	u16 pc; /* stack pointer  */
+	u16 flags; /* stack pointer  */
+	u8 memory[0x10000];
+    interupt_callback interupt[0xff];
+} Computer;
+
+
+/*
+typedef struct OpcodeOperand {
+	char name[4];
+	int  bytes;
+	int  immediate;
+	int increment;
+	int decrement;
+} OpcodeOperand;
+
+
+typedef struct OpcodeDef {
+	char mnemonic[20];
+	int num_bytes;
+	int cycle_steps;
+	int cycles[2];
+	int num_operands;
+	OpcodeOperand operands[3];
+	int immediate;
+	char flag_z;
+	char flag_n;
+	char flag_h;
+	char flag_c;
+} OpcodeDef;
+*/
+
+__declspec(dllexport)
+u32 
+computer_init(Computer *c) {
+    int i = 0;
+
+    c->ax.u16 = 0;
+    c->bx.u16 = 0;
+    c->cx.u16 = 0;
+    c->dx.u16 = 0;
+
+    c->si = 0;
+    c->di = 0;
+    c->bp = 0;
+    c->sp = 0;
+	c->pc = 0x0100;
+    c->flags = 0;
+
+    for (i = 0; i < 256; i += 1) {
+        c->interupt[i] = NULL;
+    }
+
+	puts(
+            "ax   "
+            "bx   "
+            "cx   "
+            "dx   "
+            "si   "
+            "di   "
+            "bp   "
+            "sp   "
+            "pc   "
+            "flags "
+	    );
+	return 0;
+}
+
+u16
+computer_read_u16le(Computer *gb, u16 offset) {
+	u8 low  = 0;
+	u8 high = 0;
+	u16 n16   = 0;
+
+	low  = gb->memory[offset];
+	high = gb->memory[offset + 1];
+
+	n16 = (high << 8) + low;
+
+	return n16;
+}
+
+
+
+
+
+__declspec(dllexport)
+void * 
+computer_memcpy(
+	Computer *c,
+	size_t offset,
+	const void *src,
+	size_t src_size
+	) {
+
+	void *rval  = NULL;
+	void *dst   = NULL;
+	void *end   = NULL;
+	void *limit = NULL;
+
+	limit = &c->memory[0] + sizeof(c->memory);
+
+	dst = &c->memory[offset];
+	end = dst + src_size;
+
+	if (end >= limit) {
+		printf("dst   = %p\n", dst);
+		printf("end   = %p\n", end);
+		printf("limit = %p\n", limit);
+		puts("doesnt fit");
+		return NULL;
+	}
+
+	rval = memcpy(dst, src, src_size);
+
+	return rval;
+}
+
+
+    /*
+void
+computer_dis_operand(
+				Computer *gb,
+				u16 offset,
+				char *s,
+				OpcodeOperand *op) {
+
+		u16 a16 = 0;
+		u16 n16 = 0;
+		u8  n8  = 0;
+		s8  e8  = 0;
+
+		char prefix[2] = "";
+		char suffix[2] = "";
+		char mod[2] = "";
+
+		if (op->immediate == false) {
+				prefix[0] = '[';
+				suffix[0] = ']';
+		}
+
+		if (op->increment) {
+				mod[0] = '+';
+		} else if (op->decrement) {
+				mod[0] = '-';
+		}
+
+		if (strcmp(op->name, "a16") == 0) {
+				a16 = computer_read_u16le(gb, offset+1);
+				sprintf(s, "%s0x%04x%s", prefix, a16, suffix);
+		} else if (strcmp(op->name, "n16") == 0) {
+				n16 = computer_read_u16le(gb, offset+1);
+				sprintf(s, "%s0x%04x%s", prefix, n16, suffix);
+		} else if (strcmp(op->name, "n8") == 0) {
+				n8 = computer_read_u8(gb, offset+1);
+				sprintf(s, "%s0x%02x%s", prefix, n8, suffix);
+		} else if (strcmp(op->name, "e8") == 0) {
+				n8 = computer_read_u8(gb, offset+1);
+				e8 = n8;
+				a16 = gb->pc + 2 + e8;
+				sprintf(s, "%s%04x%s", prefix, a16, suffix);
+		} else {
+				sprintf(s, "%s%s%s%s", prefix, op->name, mod, suffix);
+		}
+}
+        */
+
+    /*
+void
+computer_dis(
+	Computer *gb,
+	u16 offset,
+	char *opcode_bytes,
+	char *assembly) {
+	char sep[2] = "";
+	static char op1[10] = "";
+	static char op2[10] = "";
+	static char op3[10] = "";
+
+	u8 *pc = &gb->memory[offset];
+	OpcodeDef *spec = NULL;
+
+	if (*pc == 0xcb) {
+		spec = &prefixed[*pc];
+	} else {
+		spec = &unprefixed[*pc];
+	}
+
+	switch (spec->num_bytes) {
+	case 0:
+		sprintf(opcode_bytes, "%02x      ", *pc);
+		break;
+	case 1:
+		sprintf(opcode_bytes, "%02x      ", *pc);
+		break;
+	case 2:
+		sprintf(opcode_bytes, "%02x %02x   ", *pc, *(pc + 1));
+		break;
+	case 3:
+		sprintf(opcode_bytes,
+			"%02x %02x %02x",
+			*pc,
+			*(pc + 1),
+			*(pc + 2));
+		break;
+	default:
+		printf("num_bytes = %d\n", spec->num_bytes);
+		assert(false);
+	}
+
+	switch (spec->num_operands) {
+	case 0:
+		sprintf(assembly, "%s         ", spec->mnemonic);
+		break;
+	case 1:
+		computer_dis_operand(gb, offset, op1, &spec->operands[0]);
+		sprintf(
+			assembly,
+			"%-3s %s      ",
+			spec->mnemonic,
+			op1
+			);
+		break;
+	case 2:
+		computer_dis_operand(gb, offset, op1, &spec->operands[0]);
+		computer_dis_operand(gb, offset, op2, &spec->operands[1]);
+		sprintf(
+			assembly,
+			"%-3s %s %s   ",
+			spec->mnemonic,
+			op1,
+			op2
+			);
+		break;
+	case 3:
+		computer_dis_operand(gb, offset, op1, &spec->operands[0]);
+		computer_dis_operand(gb, offset, op2, &spec->operands[1]);
+		computer_dis_operand(gb, offset, op3, &spec->operands[2]);
+		sprintf(
+			assembly,
+			"%-3s %s %s %s",
+			spec->mnemonic,
+			op1,
+			op2,
+			op3
+			);
+		break;
+	default:
+		assert(false);
+	}
+
+}
+    */
+
+
+__declspec(dllexport)
+u32
+computer_dump(Computer *c) {
+    u16 file_offset = 0;
+    /*
+	static char opcode_bytes[9] = "op";
+	static char assembly[20] = "asm";
+	static char flags[5] = "----";
+	u8 deref_hl = gb->memory[gb->hl.u16];
+
+	computer_dis(gb, gb->pc, opcode_bytes, assembly);
+
+	flags[0] = gb->af.part.f & mask_flag_z ? 'z' : '-';
+	flags[1] = gb->af.part.f & mask_flag_h ? 'h' : '-';
+	flags[2] = gb->af.part.f & mask_flag_n ? 'n' : '-';
+	flags[3] = gb->af.part.f & mask_flag_c ? 'c' : '-';
+        */
+
+	printf(
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x "
+            "%04x"
+            "\n"
+            ,
+            c->ax.u16,
+            c->bx.u16,
+            c->cx.u16,
+            c->dx.u16,
+            c->si,
+            c->di,
+            c->bp,
+            c->sp,
+            c->pc,
+            c->flags
+		);
+	return 0;
+}
+
+
+    /*
+void
+computer_push(Computer *gb, u16 value) {
+	gb->sp -= 2;
+	gb->memory[gb->sp] = value & 0xff;
+	gb->memory[gb->sp + 1] = (value >> 8) & 0xff;
+}
+
+    */
+
+__declspec(dllexport)
+u32
+computer_step(Computer *c, u32 steps) {
+	u32 i          = 0;
+	u8 opcode      = 0;
+	u8 param       = 0;
+    u16 n8         = 0;
+    u16 n16        = 0;
+    s8  e8         = 0;
+
+	for (i = 0; i < steps; i += 1) {
+		opcode = c->memory[c->pc];
+        /* printf("opcode %x\n", opcode); */
+        switch (opcode) {
+            case 0x43: /* inc bx */
+                /* TODO */
+                c->pc += 1;
+                break;
+
+            case 0x48: /* dec ax */
+                /* TODO */
+                c->pc += 1;
+                break;
+
+            case 0x53: /* push bx */
+                /* TODO */
+                c->pc += 1;
+                break;
+
+            case 0x5b: /* pop bx */
+                /* TODO */
+                c->pc += 1;
+                break;
+
+            case 0x74: /* jz ? */
+                e8 = c->memory[c->pc+1];
+                /* TODO */
+                //c->pc += e8;
+                c->pc += 2;
+                break;
+
+            case 0x8a: /* mov al, [bx] */
+                n8 = c->memory[c->pc+1];
+                c->ax.part.al = n8;
+                c->pc += 2;
+                break;
+
+            case 0x84: /* test ? ? */
+                param = c->memory[c->pc+1];
+
+                switch (param) {
+                    case 0xc0: /* test al, al */
+                        /* TODO */
+                        c->pc += 2;
+                        break;
+
+                    default:
+                        fprintf(stderr, "unknown param 0x%02x\n\n", param);
+                        return 1;
+                }
+                break;
+
+            case 0xb4: /* mov ah, ?*/
+                // n8 = c->memory[c->pc+1];
+                //c->ax.part.ah = n8;
+                c->pc += 2;
+                break;
+
+            case 0xbb: /* mov bx, n16*/
+                n16 = computer_read_u16le(c, c->pc + 1);
+                /* printf("n16 %X\n", n16); */
+                c->bx.u16 = n16;
+                c->pc += 3;
+                break;
+
+            case 0xcd: /* int ? */
+                n8 = c->memory[c->pc+1];
+
+                if (c->interupt[n8] == NULL) {
+                    fprintf(stderr, "unknown interupt 0x%02x\n\n", n8);
+                    return 1;
+                }
+
+                if (c->interupt[n8](c)) {
+                    return 1;
+                }
+
+                c->pc += 2;
+                break;
+
+            case 0xeb: /* jmp short ? */
+                e8 = c->memory[c->pc+1];
+                /* TODO */
+                //c->pc += e8;
+                c->pc += 2;
+                break;
+
+			default:
+				fprintf(stderr, "unknown opcode 0x%02x\n\n", opcode);
+				return 1;
+        }
+
+	}
+
+	return 0;
+}
+
---
+++ emu/test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LITTLE_ENDIAN
+#include "emu.c"
+
+
+int
+interupt_0x10_putchar(Computer *c) {
+    puts("putchar");
+    return 0;
+}
+
+int
+interupt_0x20_exit(Computer *c) {
+    puts("exit");
+    return 1;
+}
+
+int
+main(int argc, char **argv) {
+    FILE *file = NULL;
+    char *filename = NULL;
+    size_t file_size = 0;
+    char *rom = NULL;
+    int i = 0;
+
+	Computer c;
+	computer_init(&c);
+    c.interupt[0x10] = interupt_0x10_putchar;
+    c.interupt[0x20] = interupt_0x20_exit;
+
+    assert(argc == 2);
+
+    filename = *(argv+1);
+    /* printf("filename: %s\n", filename); */
+
+    file = fopen(filename, "rb");
+    if (file == NULL) {
+        assert(false);
+        return 1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    rom = malloc(file_size);
+    if (rom == NULL) {
+        assert(false);
+        return 1;
+    }
+
+    fread(rom, 1, file_size, file);
+    fclose(file);
+
+    computer_memcpy(&c, 0x0100, rom, file_size);
+
+    for (i = 0; i < 100; i += 1) {
+        if (computer_step(&c, 1)) {
+            break;
+        }
+        computer_dump(&c);
+    }
+
+    computer_dump(&c);
+
+	return 0;
+}
+
